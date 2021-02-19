2bad5315c735d041005027f0cf9df99b7867151b
fitripuspitasari <fitripuspitasari_19@kharisma.ac.id>
2021-02-19 22:51:16 +0700

    hm

--- js/music.js
+++ js/music.js
@@ -9,7 +9,7 @@ function getEvents() {
       data.event.forEach(function(pa) {
         events += `
         <div class="col-lg-4">
-          <img src="frontend${pa.gambar_event}" class="img rounded-circle" width="140" height="140" alt="2005">
+          <img src="../frontend${pa.gambar_event}" class="img rounded-circle" width="140" height="140" alt="2005">
 
           <h2> ${pa.nama_event} </h2>
           <p>Info event: ${pa.info_event} </p>
@@ -46,7 +46,7 @@ function geteventsdetail(event_id) {
         <br>
         <br>
          <div class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" >
-              <img src="frontend${pt.gambar_event}" alt="">
+              <img src="../frontend${pt.gambar_event}" alt="">
          </div>
         </div>
 
